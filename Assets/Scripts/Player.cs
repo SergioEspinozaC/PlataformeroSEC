4a17b20d3ac41b2c1518ef2a795a285de0907d5c
SergioEspinozaC <sergiojose180601@gmail.com>
2022-09-15 19:15:00 -0600

    primeras animaciones y salto

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float jumpForce;
     private float horizontal;
     private Rigidbody2D rigidBody2D;
+    private Animator animator;
+    private bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,11 +23,34 @@ public class Player : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal") * speed;
         if(horizontal < 0.0f)
         {
-            transform.localScale = new Vector2(-1.0f, 1.0f);
+            transform.localScale = new Vector2(-20.0f, 20.0f);
         }else if (horizontal > 0.0f)
         {
-            transform.localScale = new Vector2(1.0f, 1.0f);
+            transform.localScale = new Vector2(20.0f, 20.0f);
         }
+
+        
+        animator.SetBool("isRunning", horizontal != 0.0f);
+
+       // Debug.DrawRay(transform.position, Vector2.down*1.1f, Color.green);
+        if (Physics2D.Raycast(transform.position, Vector2.down, 1.1f))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rigidBody2D.AddForce(Vector2.up * jumpForce);
     }
 
     private void FixedUpdate() 
